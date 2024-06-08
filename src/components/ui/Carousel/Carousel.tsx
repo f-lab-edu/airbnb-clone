f8b2a846955870e9aafb996fc757e84a84b88d96
Hyunsu <hs9880@gmail.com>
2024-06-08 23:12:54 +0900

    chore: [WIP] flex 에서 grid 변경
    - slide list, next,prev 버튼 배치 용이
    - 각각의 slide 도 grid 로 변경하여 시도 중

--- src/components/ui/Carousel/Carousel.tsx
+++ src/components/ui/Carousel/Carousel.tsx
@@ -25,12 +25,20 @@ const Carousel = ({ children }: CrouselProps) => {
     }
 
     return (
-        <>
-            <div className={'overflow-hidden gap-6'}>
+        <div
+            className={'carousel-grid carousel_container relative'}
+            ref={carouselContainerRef}
+        >
+            <div className={'area-carousel_container overflow-hidden gap-10'}>
                 <div
-                    className="carousel-track flex gap-10"
+                    ref={carouselTrackRef}
+                    className={'flex gap-6'}
                     style={{
-                        transform: `translateX(-${(currentSlide - 1) * 100}%)`,
+                        display: 'grid',
+                        gridAutoFlow: 'column',
+                        // gridTemplateColumns: ' repeat(auto-fill, 47px)',
+                        gridAutoColumns: '47px',
+                        transform: `translateX(-${translateX}px)`,
                     }}
                 >
                     {childArray.map((child, idx) => (
@@ -38,14 +46,55 @@ const Carousel = ({ children }: CrouselProps) => {
                     ))}
                 </div>
             </div>
-            <button onClick={handlePrev}>prev</button>
-            <button onClick={handleNext}>next</button>
-        </>
+            <button
+                className={
+                    'area-carousel_prev_btn flex justify-center items-center w-7 h-7 rounded-full border border-1 border-gray-20'
+                }
+                onClick={handleOnPrev}
+            >
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 32 32"
+                    width="12px"
+                    height="12px"
+                    aria-hidden="true"
+                    role="presentation"
+                    focusable="false"
+                >
+                    <path
+                        fill="none"
+                        d="M20 28 8.7 16.7a1 1 0 0 1 0-1.4L20 4"
+                        stroke="black"
+                        strokeWidth="5"
+                    ></path>
+                </svg>
+            </button>
+            <button
+                className={
+                    'area-carousel_next_btn flex justify-center items-center w-7 h-7 rounded-full border border-1 border-gray-200'
+                }
+                onClick={handleOnNext}
+            >
+                <svg
+                    width="12px"
+                    height="12px"
+                    viewBox="0 0 12 12"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        d="M4.5 1.5L8.7375 5.7375C8.80621 5.8076 8.8447 5.90184 8.8447 6C8.8447 6.09816 8.80621 6.1924 8.7375 6.2625L4.5 10.5"
+                        stroke="black"
+                        strokeWidth="2"
+                    />
+                </svg>
+            </button>
+        </div>
     )
 }
 
 const CarouselItem = ({ children }: CarouselItemProps) => {
-    return <>{children}</>
+    return <div>{children}</div>
 }
 
 Carousel.Item = CarouselItem
