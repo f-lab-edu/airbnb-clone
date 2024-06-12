ad0c0f27fd14d421b1b1b02801f5672b882081d7
Hyunsu <hs9880@gmail.com>
2024-06-12 17:20:06 +0900

    chore: react 버전 18.2 업데이트 -issue33
    - image 의 속성중 `priorityfetch` 가 타입과 리액트간의 불일치로 생겨난 리액트 이슈
    - 커뮤니티에서는 버전 업데이트로 해결 가능 (리액트 오픈소스 이슈 커밋 머지 완료)

--- src/components/CategoryBar/index.tsx
+++ src/components/CategoryBar/index.tsx
@@ -5,11 +5,11 @@ import { Carousel } from '@/components/ui/Carousel/Carousel'
 
 export const CategoryBar = () => {
     //TODO : icon과 title을 매칭하여 같이 렌더링 해줘야 한다.
-    // const categoryIconDir = path.join(process.cwd(), '/public/category_icons')
-    // const categoryIcons = fs.readdirSync(categoryIconDir)
-    const categoryIcons = Array.from({ length: 30 }).map((_, idx) => (
-        <div key={idx}> {idx}</div>
-    ))
+    const categoryIconDir = path.join(process.cwd(), '/public/category_icons')
+    const categoryIcons = fs.readdirSync(categoryIconDir)
+    // const categoryIcons = Array.from({ length: 30 }).map((_, idx) => (
+    //     <div key={idx}> {idx}</div>
+    // ))
     return (
         <Carousel>
             {categoryIcons.map((icon, idx) => (
