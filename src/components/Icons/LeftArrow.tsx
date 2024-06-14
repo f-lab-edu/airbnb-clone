092475289b14003bb9cef9a676b25857e549bd17
Hyunsu <hs9880@gmail.com>
2024-06-14 15:28:21 +0900

    chore: svgr 적용

--- src/components/Icons/LeftArrow.tsx
+++ src/components/Icons/LeftArrow.tsx
@@ -1,9 +1,22 @@
-import { SVGIcon, SVGIconProps } from '@/components/ui/SVGIcon'
-
-export const LeftArrow = (props: SVGIconProps) => {
-    return (
-        <SVGIcon viewBox={'0 0 32 32'} {...props}>
-            <path fill="none" d="M20 28 8.7 16.7a1 1 0 0 1 0-1.4L20 4"></path>
-        </SVGIcon>
-    )
-}
+import * as React from 'react'
+import type { SVGProps } from 'react'
+const SvgLeftArrow = (props: SVGProps<SVGSVGElement>) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        aria-hidden="true"
+        style={{
+            display: 'block',
+            fill: 'none',
+            height: 12,
+            width: 12,
+            stroke: 'currentcolor',
+            strokeWidth: 5.33333,
+            overflow: 'visible',
+        }}
+        viewBox="0 0 32 32"
+        {...props}
+    >
+        <path d="M20 28 8.7 16.7a1 1 0 0 1 0-1.4L20 4" />
+    </svg>
+)
+export default SvgLeftArrow
---
+++ src/components/Icons/index.ts
@@ -0,0 +1,4 @@
+export { default as Hamburger } from './Hamburger'
+export { default as LeftArrow } from './LeftArrow'
+export { default as Profile } from './Profile'
+export { default as RightArrow } from './RightArrow'
