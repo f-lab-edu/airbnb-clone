cdb9670d7abceb36a1ce952b52e2ac372e989e0c
Hyunsu <hs9880@gmail.com>
2024-06-14 15:28:21 +0900

    chore: svgr 적용

--- src/components/ProfileButton/index.tsx
+++ src/components/ProfileButton/index.tsx
@@ -1,16 +1,16 @@
 'use client'
 
-import { UserIcon } from '@/components/Icons/UserIcon'
-import { HamburgerIcon } from '@/components/Icons/HamburgerIcon'
+import UserIcon from '@/components/Icons/Profile'
+import HamburgerIcon from '@/components/Icons/Hamburger'
 
 export const ProfileButton = () => {
     return (
         <div className="flex items-center justify-center w-24 h-12 bg-white rounded-full border border-gray-200 cursor-pointer">
             <div className="flex items-center justify-center w-8 h-8 rounded-full">
-                <HamburgerIcon name={'hamburger'} size={17} />
+                <HamburgerIcon />
             </div>
             <div className="flex items-center justify-center w-8 h-8 rounded-full ml-2">
-                <UserIcon name={'user'} size={33} />
+                <UserIcon />
             </div>
         </div>
     )
