4ea36a8b96b4898498fc1712582817ee201e65ed
Hyunsu <hs9880@gmail.com>
2024-06-25 12:09:10 +0900

    feat: BaseTab 공통 컴포넌트 구현

---
+++ src/components/ui/BaseTabs/BaseTabs.tsx
@@ -0,0 +1,75 @@
+'use client'
+import React, { useContext } from 'react'
+import BaseTabsContext from './BaseTabsContext'
+import {
+    BaseTabsContentProps,
+    BaseTabsProps,
+    BaseTabsTriggerProps,
+    BaseTabsListProps,
+} from './BaseTabs.types'
+import clsx from 'clsx'
+
+//개선하고 싶은 부분 : BaseTabs에 제네릭으로 받은 T - value를 하위 컴포넌트에서 제한된 T 로 좁혀지게 사용하도록?;
+
+const BaseTabs = <T extends string>(
+    { children }: BaseTabsProps<T>,
+    ref: React.Ref<HTMLDivElement>
+) => {
+    const [activeItem, setActiveItem] = React.useState('')
+    return (
+        <BaseTabsContext.Provider
+            value={{
+                activeItem,
+                setActiveItem,
+            }}
+        >
+            {children}
+        </BaseTabsContext.Provider>
+    )
+}
+
+const BaseTabsTrigger = <T extends string>({
+    value,
+    children,
+    activeClassName,
+    ...props
+}: BaseTabsTriggerProps<T>) => {
+    const { activeItem, setActiveItem } = useContext(BaseTabsContext)
+    const handleBaseTabsTrigger = () => {
+        console.log('value', value, activeItem)
+        if (value === activeItem) return
+        setActiveItem(value)
+    }
+    return (
+        <button
+            {...props}
+            className={clsx(activeItem === value && activeClassName)}
+            onClick={handleBaseTabsTrigger}
+        >
+            {children}
+        </button>
+    )
+}
+
+const BaseTabsList = ({ children, ...props }: BaseTabsListProps) => {
+    return (
+        <div className="grid grid-cols-card-grid" {...props}>
+            {children}
+        </div>
+    )
+}
+
+const BaseTabsContent = <T extends string>({
+    value,
+    children,
+    ...props
+}: BaseTabsContentProps<T>) => {
+    const { activeItem } = useContext(BaseTabsContext)
+    return activeItem === value ? <div {...props}>{children}</div> : null
+}
+
+BaseTabs.Trigger = BaseTabsTrigger
+BaseTabs.List = BaseTabsList
+BaseTabs.Content = BaseTabsContent
+
+export { BaseTabsTrigger, BaseTabsList, BaseTabsContent, BaseTabs }
---
+++ src/components/ui/BaseTabs/index.tsx
@@ -0,0 +1,3 @@
+'use client'
+
+export { BaseTabs } from './BaseTabs'
