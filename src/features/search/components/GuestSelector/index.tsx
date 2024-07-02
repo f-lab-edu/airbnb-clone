66cc91d13a25463edc907c0c7bbb73fc4d6e6ead
Hyunsu <hs9880@gmail.com>
2024-07-03 07:17:58 +0900

    refactor:  각 탭마다  일관성있는 selector 이름으로 변경

--- src/features/search/components/GuestSelector/index.tsx
+++ src/features/search/components/GuestSelector/index.tsx
@@ -1,17 +1,15 @@
+'use client'
+
+import React from 'react'
 import { Flex } from '@/components/Flex'
 import { GuestOptionId } from '@/store/types'
 import { useSearchStore } from '@/store/useSearchStore'
-import {
-    CountInputProps,
-    GUEST_TYPES,
-    GuestCountOperation,
-} from '@/types/types'
-import React from 'react'
+import { GUEST_TYPES, GuestCountOperation } from '@/types/types'
 import { GuestTypeInfo } from '../GuestTypeInfo'
 import { GuestTypeCounter } from '../GuestTypeCounter'
 
 // 요구사항: 어린이 게스트 증가 감소할 경우, 성인으로 간주하여 증가 감소
-export const GuestCountChooser = () => {
+export const GuestSelector = () => {
     const { guests, setGuests } = useSearchStore()
 
     const handleButtonClicked = (
