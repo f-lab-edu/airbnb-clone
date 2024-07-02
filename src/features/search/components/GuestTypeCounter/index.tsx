84ef3f8f7b83b1eb73f1bf004810e3e2ec7999da
Hyunsu <hs9880@gmail.com>
2024-07-03 05:13:47 +0900

    feat: 게스트 수 UI 추가

---
+++ src/features/search/components/GuestTypeCounter/index.tsx
@@ -0,0 +1,27 @@
+import { Flex } from '@/components/Flex'
+import { CountInputProps } from '@/types/types'
+
+export const GuestTypeCounter = ({
+    onButtonClick,
+    count,
+    guestType,
+}: CountInputProps) => {
+    return (
+        <Flex
+            justify={'justify-between'}
+            align={'center'}
+            className={'min-w-96'}
+        >
+            <button
+                name="subtract"
+                onClick={(e) => onButtonClick(guestType, e)}
+            >
+                -
+            </button>
+            <span>{count}</span>
+            <button name="add" onClick={(e) => onButtonClick(guestType, e)}>
+                +
+            </button>
+        </Flex>
+    )
+}
---
+++ src/types/types.ts
@@ -0,0 +1,28 @@
+import { GuestOptionId } from '@/store/types'
+
+export type CountInputProps = {
+    onButtonClick: (
+        guestType: GuestOptionId,
+        e: React.MouseEvent<HTMLButtonElement>
+    ) => void
+    count: number
+    guestType: GuestOptionId
+}
+
+export const GUEST_TYPES: {
+    [k in GuestOptionId]: { label: string; ageRange: string }
+} = {
+    adults: { label: '성인', ageRange: '13세 이상' },
+    children: { label: '어린이', ageRange: '2~12세' },
+    infants: { label: '유아', ageRange: '2세 미만' },
+    pets: { label: '반려동물', ageRange: '' },
+}
+
+export type GuestCountOperation = 'add' | 'subtract'
+
+export const Guest = {
+    adults: 'adults',
+    children: 'children',
+    infants: 'infants',
+    pets: 'pets',
+} as const
