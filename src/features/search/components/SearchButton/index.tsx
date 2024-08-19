3e2bbcc07475b9669e7cbc40d6fa4879b1f248de
Hyunsu <hs9880@gmail.com>
2024-08-19 23:52:40 +0900

    feat: 서치 버튼 이벤트 핸들러를 props 전달 로직 추가

--- src/features/search/components/SearchButton/index.tsx
+++ src/features/search/components/SearchButton/index.tsx
@@ -1,13 +1,18 @@
 'use client'
 import Search from '@/components/Icons/Search'
 
-export const SearchButton = () => {
+export const SearchButton = ({
+    onSearchButtonClicked,
+}: {
+    onSearchButtonClicked: () => void
+}) => {
     return (
         <div>
             <button
                 className={
                     'flex justify-center items-center w-12 h-12 rounded-full bg-rose-600 mr-4'
                 }
+                onClick={onSearchButtonClicked}
             >
                 <Search width={14} height={14} />
             </button>
